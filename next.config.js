27d8dea66e4fe8617a0fd4816c2bb2054dc54bac
lilsouris <hithomasjay@gmail.com>
2025-10-06 16:30:11 +0200

    Enable production browser source maps to locate client-side error origin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Remove experimental features that require canary version
+  productionBrowserSourceMaps: true,
 };
 
 module.exports = nextConfig;
